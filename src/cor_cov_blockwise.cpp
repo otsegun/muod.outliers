568b175db0e727622cd197e38e2514d4eec48161
Luis F. Chiroque <luisfo89@gmail.com>
2016-11-24 14:39:06 +0100

    source cov_c (c++) cleaned, commented and formated

--- src/cor_cov_blockwise.cpp
+++ src/cor_cov_blockwise.cpp
@@ -11,41 +11,49 @@ using namespace Rcpp;
 using namespace std;
 
 inline NumericMatrix c_cov_helper(const NumericMatrix& mat, const int rstart, const int rend
-	, NumericVector means, NumericVector vars, NumericVector sds
-	, const int cstart, const int cend) {
-	int nc = mat.ncol();
-	int nperiod = rend - rstart;
-	int ksize = cend - cstart + 1;
-	NumericMatrix routl(ksize, 3);
+  , NumericVector means, NumericVector vars, NumericVector sds
+  , const int cstart, const int cend) {
+  const int nc = mat.ncol();
+  const int nperiod = rend - rstart;
+  const int ksize = cend - cstart + 1;
+  NumericMatrix routl(ksize, 3);
 
-	// pre-compute outliers
-	for (int row = 0; row < nc; row++) {
-		for (int col = 0; col < ksize; col++) {
-			double sXY = 0;
-			double aux;
+  // pre-compute outliers
+  for (int cur = 0; cur < nc; cur++) {
+    for (int col = 0; col < ksize; col++) {
+      long double sXY = 0;
+      long double aux;
 
-			for (int r = rstart; r < rend; r++)
-	    			sXY += mat(r, row) * mat(r, col + cstart);
-		   	aux = sXY / (nperiod - 1);
-			routl(col, 0) += (aux / sds[row] - routl(col, 0)) / (row+1); //aggregated mean
-			routl(col, 1) += (aux * (means[row] / vars[row]) - routl(col, 1)) / (row+1); //aggregated mean
-			//routl(col, 2) += (aux / vars[row] - routl(col, 0)) / (row+1);
-			routl(col, 2) += aux / vars[row];
-		}
-	}
-	// perform mean over routl
-	for (int col = 0; col < ksize; col++) {
-		//routl(col, 0) /= nc;
-		//routl(col, 1) /= nc;
-		routl(col, 2) /= nc;
-	}
-	return routl;
+      for (int r = rstart; r < rend; r++)
+        sXY += mat(r, cur) * mat(r, col + cstart);
+      aux = sXY / (nperiod - 1);
+      // moving average
+      routl(col, 0) += (aux / sds[cur] - routl(col, 0)) / (cur+1); //aggregated mean
+      routl(col, 1) += (aux * (means[cur] / vars[cur]) - routl(col, 1)) / (cur+1); //aggregated mean
+      routl(col, 2) += (aux / vars[cur] - routl(col, 2)) / (cur+1);
+      /* classical average; step 1, accumulate
+      // average (sum)
+      routl(col, 0) += aux / sds[cur]; //aggregated mean
+      routl(col, 1) += aux * (means[cur] / vars[cur]); //aggregated mean
+      routl(col, 2) += aux / vars[cur];
+      */
+    }
+  }
+  // perform mean over routl
+  /* classical average; step 2, divide by nc
+  for (int col = 0; col < ksize; col++) {
+    routl(col, 0) /= nc;
+    routl(col, 1) /= nc;
+    routl(col, 2) /= nc;
+  }
+  */
+  return routl;
 }
 
 // [[Rcpp::export]]
 NumericMatrix c_cov(NumericMatrix mat
-	, NumericVector means, NumericVector vars, NumericVector sds
-	, int c_ini, int c_end) {
-	return c_cov_helper(mat, 0, mat.nrow(), means, vars, sds, c_ini-1, c_end-1);
+  , NumericVector means, NumericVector vars, NumericVector sds
+  , int c_ini, int c_end) {
+  return c_cov_helper(mat, 0, mat.nrow(), means, vars, sds, c_ini-1, c_end-1);
 }
 
